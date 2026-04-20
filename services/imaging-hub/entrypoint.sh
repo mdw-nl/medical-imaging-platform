0012e43cee04df62175a6c071215c9b4ca218cf9
alessioromita <romitaalessio@gmail.com>
2026-04-20 14:42:07 +0200

    Remove constrain of using sudo to delete the deploy/data folder

--- services/imaging-hub/entrypoint.sh
+++ services/imaging-hub/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
-chown app:app /dicomsorter/data
-chown app:app /dicom-staging-overflow
-exec gosu app "$@"
+TARGET_UID=${HOST_UID:-$(id -u app)}
+TARGET_GID=${HOST_GID:-$(id -g app)}
+
+chown ${TARGET_UID}:${TARGET_GID} /dicomsorter/data
+chown ${TARGET_UID}:${TARGET_GID} /dicom-staging-overflow
+exec gosu ${TARGET_UID}:${TARGET_GID} "$@"
