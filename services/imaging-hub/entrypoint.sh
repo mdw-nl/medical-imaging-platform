eacfea4d05966b43e1be7ab34b749ef490f9bfac
alessioromita <romitaalessio@gmail.com>
2026-04-21 11:36:44 +0200

    Revert "Remove constrain of using sudo to delete the deploy/data folder"
    This reverts commit 0012e43cee04df62175a6c071215c9b4ca218cf9.

--- services/imaging-hub/entrypoint.sh
+++ services/imaging-hub/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-TARGET_UID=${HOST_UID:-$(id -u app)}
-TARGET_GID=${HOST_GID:-$(id -g app)}
-
-chown ${TARGET_UID}:${TARGET_GID} /dicomsorter/data
-chown ${TARGET_UID}:${TARGET_GID} /dicom-staging-overflow
-exec gosu ${TARGET_UID}:${TARGET_GID} "$@"
+chown app:app /dicomsorter/data
+chown app:app /dicom-staging-overflow
+exec gosu app "$@"
