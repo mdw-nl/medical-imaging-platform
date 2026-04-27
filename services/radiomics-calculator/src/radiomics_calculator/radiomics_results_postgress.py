d544aa950c1e5533b2f526d837e006220c5adeff
TomSinsel <sinseltom@gmail.com>
2026-04-27 21:39:39 +0200

    Extended radiomics params, updated normalization, fixed XNAT routing issue, fixed radiomics results

--- services/radiomics-calculator/src/radiomics_calculator/radiomics_results_postgress.py
+++ services/radiomics-calculator/src/radiomics_calculator/radiomics_results_postgress.py
@@ -236,8 +236,8 @@ def send_postgress(db, csv_content, metadata):
     radiomics_id = str(uuid.uuid4())  # generates a unique ID as a string
     manager_data = {}
 
-    manager_data["radiomics_id"] = (radiomics_id,)
-    manager_data["sop_instance_uid"] = (metadata["sop_instance_uid"],)
+    manager_data["radiomics_id"] = radiomics_id
+    manager_data["sop_instance_uid"] = metadata["sop_instance_uid"]
 
     try:
         db.insert("radiomics_manager", manager_data)
